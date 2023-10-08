f3e7a8890319b6beb067464f215df6744c8077e8
Insane30 <akaayushkachhap30@gmail.com>
2023-10-08 10:43:11 +0530

    Enhance speech recognition and add note viewing feature

--- main.py
+++ main.py
@@ -1,45 +1,52 @@
 import speech_recognition as sr
 
-# empty string
-note = ""
+# Initialize an empty list to store notes
+notes = []
 
 # Functions
 def takeNotes():
-    '''It takes microphone input from the user and returns a string output'''
+    '''It takes microphone input from the user and appends it to the notes list'''
 
-    global note
-    recogniser = sr.Recognizer()
+    global notes
+    recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        recogniser.pause_threshold = 1
-        audio = recogniser.listen(source)
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(source)
 
         try:
             print("Recognizing...")
-            note = recogniser.recognize_google(audio, language="en-in")
-            # print(note)
-        except Exception as e:
-            print(e)
+            note = recognizer.recognize_google(audio, language="en-in")
+            notes.append(note)  # Append the note to the list
+            print("Note written!")  # Print "Note written" after each note is recognized
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def writeNotes():
-    '''Writing the notee in the text file'''
+def writeNotesToFile():
+    '''Writing the notes to a text file'''
 
-    global note
-    with open("notes.txt", "a+") as file:
-        file.writelines(note)
-        file.writelines("\n")
-        print("Notes written !")
-
+    global notes
+    if notes:
+        with open("notes.txt", "a+") as file:
+            for note in notes:
+                file.write(note + "\n")
+        print("All notes saved to file.")
 
 if __name__ == '__main__':
-    takeNotes()
-    writeNotes()
-
     while True:
-        ask = input("Do you want to take more inputs?[y/N]: ")
-        if ask == "y":
-            takeNotes()
-            writeNotes()
-        if ask == "N":
-            print("Thanks For using us !")
+        takeNotes()
+        ask = input("Do you want to take more notes? (y/N): ")
+        if ask.lower() != "y":
+            writeNotesToFile()
+            print("Thanks for using us!")
             break
+
+    ask = input("Do you want to view the notes? (y/N): ")
+    if ask.lower() == "y":
+        # Read and display notes from the file
+        with open("notes.txt", "r") as file:
+            print("Your Notes:")
+            for line in file:
+                print(line.strip())  # Strip newline characters and print each note
